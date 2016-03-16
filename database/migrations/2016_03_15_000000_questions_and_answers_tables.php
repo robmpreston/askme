ee5a2f1960f4b2fdbe8cdf05b8425d018db248fc
Jon Robinson <jon.f.robinson@gmail.com>
2016-03-15 21:27:06 -0400

    work in progress

---
+++ database/migrations/2016_03_15_000000_questions_and_answers_tables.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class QuestionsAndAnswersTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('to_user_id')->unsigned()->references('id')->on('users');
+            $table->integer('from_user_id')->unsigned()->references('id')->on('users');
+            $table->string('user_from')->nullable();
+            $table->string('text');
+            $table->integer('weight')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('question_id')->unsigned()->references('id')->on('questions');
+            $table->integer('user_id')->unsigned()->references('id')->on('users');
+            $table->string('text')->nullable();
+            $table->string('video_url')->nullable();
+            $table->boolean('is_video')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+        Schema::drop('answers');
+    }
+}
