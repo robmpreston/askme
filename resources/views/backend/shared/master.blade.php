ba64630fac5433f05ca891a3598a4f1302b805bc
Robert Preston <robertpreston@Roberts-MacBook-Pro.local>
2016-03-15 20:06:03 -0400

    Hi @jon and @derek

--- resources/views/backend/shared/master.blade.php
+++ resources/views/backend/shared/master.blade.php
@@ -1,12 +1,12 @@
 <html>
-    @include('backend.shared.head)
+    @include('backend.shared.head')
     <body>
-        @section('sidebar')
-
-        @show
+        @include('backend.shared.sidebar')
 
         <div class="container">
             @yield('content')
         </div>
+
+        @include('backend.shared.footer')
     </body>
 </html>
