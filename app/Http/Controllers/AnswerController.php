3c3438a7658dc8eb131ef0d9b662b75d77854623
Jon Robinson <jon.f.robinson@gmail.com>
2016-03-17 06:12:36 -0400

    storing answer

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -1,9 +1,6 @@
-<?php
-
-namespace App\Http\Controllers;
+<?php namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
 class AnswerController extends Controller
@@ -15,5 +12,26 @@ class AnswerController extends Controller
             'success' => true,
             'error' => null
         ];
+    }
+
+    /**
+     * AJAX REQUEST 
+     * If Validation Fails Json Response w/ Errors
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'respondent_id' => 'required|integer',
+            'question_id' => 'required|integer',
+            'video_url' => 'max:255',
+        ]);
+
+        $question = Question::find($request->question_id);
+        $answer = Answer::makeOne($question, [
+        	'text_response' => $request->text_response,
+        	'is_video' => $request->video_url ? true : false,
+        	'video_url' => $request->video_url,
+        ]);
+        // return view
     }
 }
