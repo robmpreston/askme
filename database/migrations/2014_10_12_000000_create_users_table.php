3e4eec3a399be3a11de7bcf56c656d10d95d4d86
Jon Robinson <jon.f.robinson@gmail.com>
2016-03-18 18:38:59 -0400

    fixing migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->string('from')->nullable();
             $table->string('picture')->nullable();
-            $table->string('i_am_a')->nullable();
             $table->string('facebook_id')->nullable();
             $table->string('twitter_id')->nullable();
             $table->boolean('respondant')->default(0); // can this person take questions?
@@ -33,7 +33,7 @@ class CreateUsersTable extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->references('id')->on('users');
-            $table->string('from')->nullable();
+            $table->string('i_am_a')->nullable();
             $table->text('description')->nullable();
 
             // websites + social media
