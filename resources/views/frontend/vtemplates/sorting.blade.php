ac47bd1219db20ac7853adf2b5869cffe6a9e124
Rob Preston <robmpreston@gmail.com>
2016-03-23 16:25:48 -0400

    Add rating

--- resources/views/frontend/vtemplates/sorting.blade.php
+++ resources/views/frontend/vtemplates/sorting.blade.php
@@ -3,5 +3,6 @@
         <li><a href="#" @click="sort('trending')" :class="{active: sortType == 'trending'}">Trending</a></li>
         <li><a href="#" @click="sort('answered')" :class="{active: sortType == 'answered'}">Answered</a></li>
         <li><a href="#" @click="sort('date')" :class="{active: sortType == 'date'}">Date</a></li>
+        <li><a href="#" @click="sort('rating') :class={active: sortType == 'rating' }">Rating</a></li>
     </ul>
 </script>
