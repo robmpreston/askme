d67a9e62b86a3cf96559ea11f8517abb9499f6b7
Rob Preston <robmpreston@gmail.com>
2016-03-24 12:39:59 -0400

    Add moment & vue-moment

--- resources/views/frontend/shared/head.blade.php
+++ resources/views/frontend/shared/head.blade.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <script src="{{ asset('bower_components/vue/dist/vue.min.js') }}"></script>
     <script src="{{ asset('bower_components/vue-resource/dist/vue-resource.min.js') }}"></script>
+    <script src="{{ asset('bower_components/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('bower_components/vue-moment/vue-moment.min.js') }}"></script>
     <script>
         var recipient = <?php echo json_encode($recipient); ?>;
         var questions = <?php echo json_encode($questions); ?>;
